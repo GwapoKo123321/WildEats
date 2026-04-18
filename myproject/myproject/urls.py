b3661f312c66c9c6db1ac2be0abc8691f25c23cf
gwapoko123321 <paradelalawrence29@gmail.com>
2026-04-18 21:31:47 +0800

    Adjusted the forms

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -2,6 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    # The correct attribute is admin.site.urls
     path('admin/', admin.site.urls),
+
+    # Requirement 2: Your App Name in the URL
+    path('operations/', include('operations.urls')),
+
+    # Root dashboard
     path('', include('operations.urls')),
 ]
